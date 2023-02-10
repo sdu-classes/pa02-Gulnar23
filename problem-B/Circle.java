8263d0e263c6b702af79732f3aaa718810bf9013
Gulnar23 <124436873+Gulnar23@users.noreply.github.com>
2023-02-10 20:22:17 +0600

    Problem-B

---
+++ problem-B/Circle.java
@@ -0,0 +1,27 @@
+class Circle extends Shape {
+    double radius;
+
+    public Circle() {
+
+    }
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius=radius;
+    }
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+    public String toString() {
+        return "Circle[Shape[color = " + color + ", filled =" + filled + "], radius = " + radius + "]";
+    }
+}
